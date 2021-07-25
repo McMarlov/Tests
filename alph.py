f7b51cb61e0f97339eeb4010da7b171044e75d86
MaxMakela <johnhopeful11@gmail.com>
2021-07-25 17:36:30 +0300

    Update alph.py

--- alph.py
+++ alph.py
@@ -1,4 +1,6 @@
 chislo = 'XXII'
+# Вместо этих двух строк можно: 
+# spisoc = list(spisoc).append(None)
 spisok = [i for i in chislo]
 spisok.append(None)
 itog = 0
@@ -12,10 +14,14 @@ slovar = {
     'M' : 1000,
 }
 def rim(numb:int):
+    # Можно было не создавать переменную arab
     arab = slovar[spisok[numb]]
     return arab
 
 for i in range(len(spisok)-1):
+    # Если сравниваешь с None нужно писать spisoc is not None
+    # Но в этом случае тебе и это не нужно, достаточно просто
+    # if spisok[i+1]: 
     if spisok[i+1]!= None:
         if rim(i) < rim(i+1):            
             itog-=rim(i)
