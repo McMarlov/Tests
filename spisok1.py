88fe7e23613493d18db251562b385c298ee57407
McMarlov <77973979+McMarlov@users.noreply.github.com>
2021-08-23 15:51:29 +0300

    Add files via upload

--- spisok1.py
+++ spisok1.py
@@ -1,29 +1,27 @@
 nums_convertation = [5, '65', '5f8', '-1', ['6'], [0], [6, 7], 'fgdg']  # Входные данные
 
 
-def convert(spisok1: list):
-    itog = []
+def convert(spisok1: list) -> list:
+    itog: list = []
     for param1 in spisok1:
-        if type(param1) is list:  # Проверяем списки
+        if isinstance(param1, list):  # Проверяем списки
             try:
-                itog.append(int(param1[0] if len(param1) < 2 else None))
-            except:
+                itog.append(int(param1[0] if len(param1) < 2 else ''))
+            except ValueError:
                 itog.append(None)
-        elif type(param1) is str:  # Проверяем строки
+        elif isinstance(param1, str):  # Проверяем строки
             try:
                 itog.append(int(param1))
-            except:
-                bufer = ''
+            except ValueError:
+                bufer: str = ''
                 for param2 in param1:
-                    try:
+                    if param2.isdigit():
                         bufer += str(int(param2))
-                    except:
-                        pass
                 try:
                     itog.append(int(bufer))
-                except:
+                except ValueError:
                     itog.append(None)
-        elif type(param1) is int:  # Проверяем числа
+        elif isinstance(param1, int):  # Проверяем числа
             itog.append(int(param1))
         else:
             itog.append(None)  # Записываем остальные элементы
