03819fd46cf42a8f48a691b73b5a0691786903b9
McMarlov <77973979+McMarlov@users.noreply.github.com>
2021-08-23 16:26:51 +0300

    Add files via upload

--- spisok1.py
+++ spisok1.py
@@ -13,14 +13,12 @@ def convert(spisok1: list) -> list:
             try:
                 itog.append(int(param1))
             except ValueError:
-                bufer: str = ''
-                for param2 in param1:
-                    if param2.isdigit():
-                        bufer += str(int(param2))
+                buffer = ''.join([i for i in param1 if i.isdigit()])
                 try:
-                    itog.append(int(bufer))
+                    buffer = int(buffer)
                 except ValueError:
-                    itog.append(None)
+                    buffer = None
+                itog.append(buffer)
         elif isinstance(param1, int):  # Проверяем числа
             itog.append(int(param1))
         else:
